87be39339e9f6697773d0355046715c9d0591e31
Yasunori MAHATA <mahata777@gmail.com>
2024-08-06 08:15:49 +0900

    :sparkles: Add React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <div>main</div>,
+	},
+	{
+		path: "/sample",
+		element: <div>Sample</div>,
+	}
+]);
 
 // biome-ignore lint/style/noNonNullAssertion: There's always a root element
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>,
 );
