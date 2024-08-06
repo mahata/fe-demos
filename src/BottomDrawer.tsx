6dc8a7c8d63f41548a2cb182f6b01165daee372a
Yasunori MAHATA <mahata777@gmail.com>
2024-08-07 08:59:02 +0900

    :sparkles: Add buttom drawer

--- src/BottomDrawer.tsx
+++ src/BottomDrawer.tsx
@@ -1,5 +1,24 @@
+import React from "react";
+
 export default function BottomDrawer() {
+	const [showBottomDrawer, setShowBottomDrawer] = React.useState(false);
+
 	return (
-		<div className="bg-blue-500 text-white p-4">Bottom Drawer abc def</div>
+		<>
+			<div className="p-2">
+				<button
+					type="button"
+					className="bg-green-500 text-white p-2 rounded"
+					onClick={() => setShowBottomDrawer((prev) => !prev)}
+				>
+					Show/Hide Drawer
+				</button>
+			</div>
+			<div
+				className={`fixed bottom-0 left-1/2 transform -translate-x-1/2 bg-blue-500 text-white p-4 ${showBottomDrawer ? "animate-slide-up opacity-100" : "opacity-0"}`}
+			>
+				Bottom Drawer
+			</div>
+		</>
 	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			keyframes: {
+				"slide-up": {
+					"0%": { transform: "translate(-50%, 100%)" },
+					"100%": { transform: "translate(-50%, 0)" },
+				},
+			},
+			animation: {
+				"slide-up": "slide-up 1s ease-out",
+			},
+		},
+	},
+	plugins: [],
+};
